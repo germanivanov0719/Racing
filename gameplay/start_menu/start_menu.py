811f7f4d731fad02807fd8a8ec570cc66df73e7e
logeval <loginova032@gmail.com>
2021-12-20 00:41:12 +0300

    change import font

--- gameplay/start_menu/start_menu.py
+++ gameplay/start_menu/start_menu.py
@@ -11,24 +11,24 @@ import gameplay.car_menu.car_menu
 from main import VERSION
 
 # Game constants
-from resources.fonts.FONTS import COMIC_SANS_REGULAR, COMIC_SANS_BOLD, COMIC_SANS_ITALIC
+from resources.fonts.FONTS import ORBITRON_MEDIUM, ORBITRON_REGULAR, ORBITRON_EXTRA_BOLD
 
 
 class StartMenu:
     def __init__(self):
         # Defining the print
         # 1. Heading
-        font = pygame.font.Font(COMIC_SANS_BOLD, 70)
+        font = pygame.font.Font(ORBITRON_EXTRA_BOLD, 70)
         self.heading = font.render("Racing", True, pygame.Color("red"))
         self.heading_x = self.heading.get_width()
         self.heading_y = self.heading.get_height()
         # 2. Info
-        font = pygame.font.Font(COMIC_SANS_REGULAR, 30)
+        font = pygame.font.Font(ORBITRON_REGULAR, 30)
         self.info = font.render(f"Version: {VERSION}", True, pygame.Color("red"))
         self.info_x = self.info.get_width()
         self.info_y = self.info.get_height()
         # 3. Play button
-        font = pygame.font.Font(COMIC_SANS_BOLD, 65)
+        font = pygame.font.Font(ORBITRON_MEDIUM, 65)
         self.play = font.render("Play", True, pygame.Color("green"))
         self.play_x = self.play.get_width()
         self.play_y = self.play.get_height()
