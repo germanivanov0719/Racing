284477e84ce6f5f5b882538b4c0945cbc1732083
logeval <loginova032@gmail.com>
2022-01-04 18:09:02 +0300

    Highway texture
    -add new highway texture
    -create a function to resize new texture

--- resources/Highways/Textures/TEXTURES.py
+++ resources/Highways/Textures/TEXTURES.py
@@ -1,3 +1,4 @@
 ASPHALT_1 = 'resources/Highways/Textures/asphalt_1.png'
 DIRT_1 = 'resources/Highways/Textures/dirt_1.png'
 SNOW_1 = 'resources/Highways/Textures/snow_1.png'
+asph = 'resources/Highways/Textures/asphalt-1.jpg'
