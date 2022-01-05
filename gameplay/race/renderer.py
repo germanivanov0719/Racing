cd124e4cd375ca439cc95dc17461cdf881a5895f
logeval <loginova032@gmail.com>
2022-01-05 16:36:33 +0300

    Update renderer.py
    render car

--- gameplay/race/renderer.py
+++ gameplay/race/renderer.py
@@ -24,18 +24,26 @@ from resources.fonts.FONTS import ORBITRON_REGULAR, ORBITRON_MEDIUM, ORBITRON_EX
 class Renderer:
     def __init__(self, screen):
         self.screen = screen
+        self.width = self.screen.get_width()
+        self.height = self.screen.get_height()
+        self.render_play_car()
         # self.clock = pygame.
 
     def load_image(self, name):
         image = pygame.image.load(name)
         return image
-
     def render_background(self):
-        self.width = self.screen.get_width()
-        self.height = self.screen.get_height()
         background = pygame.transform.scale(self.load_image('resources/Highways/Textures/road1.png'),
                                         (self.width, self.height))
         self.screen.blit(background, (0, 0))
+
+    def render_play_car(self):
+        self.play_car = self.load_image('resources/Vehicles/Textures/bus_1.png')
+        self.rect = self.play_car.get_rect().move(self.width // 2, self.height - self.height // 2.7)
+        # TODO: draw car
+
+
+
 
 
         # pygame.draw.rect(self.screen, pygame.Color('green'), (1, 1, 100, 100))
