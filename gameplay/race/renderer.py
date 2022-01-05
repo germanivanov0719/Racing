dc1f53c30281517b0c60d7aba4e4631b333d3b76
logeval <loginova032@gmail.com>
2022-01-05 16:19:18 +0300

    add highway background

--- gameplay/race/renderer.py
+++ gameplay/race/renderer.py
@@ -20,10 +20,24 @@ from main import VERSION
 # Game constants
 from resources.fonts.FONTS import ORBITRON_REGULAR, ORBITRON_MEDIUM, ORBITRON_EXTRA_BOLD
 
+
 class Renderer:
     def __init__(self, screen):
         self.screen = screen
+        # self.clock = pygame.
+
+    def load_image(self, name):
+        image = pygame.image.load(name)
+        return image
 
     def render_background(self):
-        pygame.draw.rect(self.screen, pygame.Color('green'), (1, 1, 100, 100))
+        self.width = self.screen.get_width()
+        self.height = self.screen.get_height()
+        background = pygame.transform.scale(self.load_image('resources/Highways/Textures/road1.png'),
+                                        (self.width, self.height))
+        self.screen.blit(background, (0, 0))
+
+
+        # pygame.draw.rect(self.screen, pygame.Color('green'), (1, 1, 100, 100))
+
 
