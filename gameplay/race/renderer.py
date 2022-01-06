fabf2e3c87641876cd48f4a67a50fd0427d4f4ab
logeval <loginova032@gmail.com>
2022-01-06 16:31:34 +0300

    Update renderer.py
    render background

--- gameplay/race/renderer.py
+++ gameplay/race/renderer.py
@@ -26,6 +26,11 @@ class Renderer:
         self.screen = screen
         self.width = self.screen.get_width()
         self.height = self.screen.get_height()
+        self.y1 = -settings.selected_highway.get_height()
+        self.y2 = 2 * self.y1
+        self.back = settings.selected_highway.get_texture(width=self.width)
+        self.scroll = [self.back, self.back, self.back]
+        self.move = 0
         # self.clock = pygame.
 
     def load_image(self, name):
@@ -33,12 +38,40 @@ class Renderer:
         return image
 
     def render_background(self):
-        self.screen.blit(settings.selected_highway.get_texture(width=self.screen.get_width()), (0, 0))
+        self.screen.blit(self.back, (0, self.move))
+        for i in range(len(self. scroll)):
+            if i == 0:
+                self.screen.blit(self.scroll[i], (0, self.move))
+            elif i == 1:
+                self.screen.blit(self.scroll[i], (0, self.y1 + 5))
+                self.y1 += 5
+                self.move += 5
+            else:
+                self.screen.blit(self.scroll[i], (0, self.y2 + 5))
+                self.y2 += 5
+            if self.move == self.height:
+                self.move = 0
+                self.y1 = -settings.selected_highway.get_height()
+                self.y2 = -settings.selected_highway.get_height() * 2
+
+
+
+
+
+
+
+
+
+
 
     def render_player_car(self):
         settings.vehicles.draw(self.screen)
         # settings.selected_car.check_collisions()
         # TODO: draw car
+
+    # def update_background(self):
+
+
 
 
 class AsyncRenderer:
